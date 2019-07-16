0dd16bc19d52ca9cfece45960cee52d7d419aafa
yunver <yunus.unver1995@gmail.com>
2019-07-16 21:44:24 +0300

    Added register component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import {NgxGalleryModule} from 'ngx-gallery';
 import { CityAddComponent } from './city/city-add/city-add.component';
 import {FormsModule,ReactiveFormsModule} from '@angular/forms';
 import {AlertifyService} from './services/alertify.service';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
    declarations: [
@@ -22,7 +23,8 @@ import {AlertifyService} from './services/alertify.service';
       NavComponent,
       CityComponent,
       CityDetailComponent,
-      CityAddComponent
+      CityAddComponent,
+      RegisterComponent
    ],
    imports: [
       BrowserModule,
@@ -32,7 +34,9 @@ import {AlertifyService} from './services/alertify.service';
       FormsModule,
       ReactiveFormsModule
    ],
-   providers: [AlertifyService],
+   providers: [
+      AlertifyService
+   ],
    bootstrap: [
       AppComponent
    ]
--- src/app/routes.ts
+++ src/app/routes.ts
@@ -3,9 +3,11 @@ import { ValueComponent } from './value/value.component';
 import { Routes } from '@angular/router';
 import { CityDetailComponent } from './city/city-detail/city-detail.component';
 import { CityAddComponent } from './city/city-add/city-add.component';
+import { RegisterComponent } from './register/register.component';
 
 export const appRoutes:Routes=[
     {path:"city",component:CityComponent},
+    {path:"register",component:RegisterComponent},
     {path:"value",component:ValueComponent},
     {path:"cityadd",component:CityAddComponent},
     {path:"cityDetail/:cityId",component:CityDetailComponent},
